23dca69f13894273f3175fbdbb048feecbdf60c0
DavidQuijano <aqc1@hotmail.com>
2016-05-15 03:02:37 +0100

    add configuration

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,3 +1,6 @@
+#ActionMailer Config
+config.action_mailer.default_url_options = { :host => 'example.com' }
+
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
